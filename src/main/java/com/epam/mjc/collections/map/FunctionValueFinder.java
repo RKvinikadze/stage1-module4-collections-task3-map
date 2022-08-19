bab3365af6d43b534e2e23e7b5a88683a879281c
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-08-19 08:18:26 +0400

    maps

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+        for (int x: functionMap.keySet()){
+            if (x == requiredValue)
+                return true;
+        }
+
+        return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> res = new HashMap<>();
+        for (int x: sourceList){
+            res.put(x, 5 * x + 2);
+        }
+
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> res = new HashMap<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch: sentence.toLowerCase().toCharArray()){
+            if ('a' <= ch && ch <= 'z'){
+                sb.append(ch);
+            }else{
+                if (sb.toString().isEmpty()) continue;
+                res.put(sb.toString(), res.getOrDefault(sb.toString(), 0) + 1);
+                sb = new StringBuilder();
+            }
+        }
+
+        return res;
     }
 }
